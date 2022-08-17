d42b00f1d739122ae6b0f8cc5957d8a531128e43
Ankit Kumar <76895565+ankitkumar0624@users.noreply.github.com>
2022-08-17 11:13:32 +0530

    Time: 73 ms (90.16%), Space: 48.4 MB (45.07%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int jumps=0;
+        for(int i=n-2;i>=0;i--){
+            jumps++;
+            if(nums[i]>=jumps){
+                jumps=0;
+            }
+        }
+        return jumps==0;
+    }
+};

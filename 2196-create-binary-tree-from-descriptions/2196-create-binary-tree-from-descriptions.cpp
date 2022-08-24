a5991ef8f18aa648abfd83638910f8acdb0daf58
Ankit Kumar <76895565+ankitkumar0624@users.noreply.github.com>
2022-08-24 22:28:26 +0530

    Time: 1898 ms (31.31%), Space: 337.9 MB (5.23%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -1,11 +1,13 @@
+
 class Solution {
 public:
     unordered_map<int,int>left,right;
     TreeNode* createBinaryTree(vector<vector<int>>& des) {
         unordered_set<int>c;
-        int value=-1;
         for(auto x:des)c.insert(x[1]);
-        for(auto x:des)if(c.find(x[0])==c.end()) value=x[0];
+        int head=-1;
+        for(auto x:des)if(c.find(x[0])==c.end())head=x[0];
+        TreeNode*root=new TreeNode(head);
         for(auto x:des){
             if(x[2]==1){
                 left[x[0]]=x[1];
@@ -13,18 +15,16 @@ public:
                 right[x[0]]=x[1];
             }
         }
-        TreeNode*root=new TreeNode(value);
-        fun(root);
+        create(root);
         return root;
     }
-    void fun(TreeNode*root){
-        if(root==NULL) return;
+    void create(TreeNode*root){
+        if(root==NULL)return;
         int x=root->val;
         if(left.find(x)!=left.end())root->left=new TreeNode(left[x]);
         if(right.find(x)!=right.end())root->right=new TreeNode(right[x]);
-        if(root->left==NULL&&root->right==NULL) return;
-        fun(root->left);
-        fun(root->right);
+        create(root->left);
+        create(root->right);
         return ;
     }
 };

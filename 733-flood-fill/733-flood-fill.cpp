4638bdb4b9a977884ce1ea5245ba99a1ea016d1c
Ankit Kumar <76895565+ankitkumar0624@users.noreply.github.com>
2022-09-05 20:50:56 +0530

    Time: 12 ms (70.88%), Space: 14.3 MB (25.23%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& I, int sr, int sc, int C) {
-        int val=I[sr][sc];
-        dfs(I,sr,sc,C,val);
+        int r=I.size(),c=I[0].size(),val=I[sr][sc];
+        queue<pair<int,int>>q;
+        int dir[4][2]={{0,1},{0,-1},{1,0},{-1,0}};
+        if(val==C) return I;
+        I[sr][sc]=C;
+        q.push({sr,sc});
+        while(!q.empty()){
+            int x=q.front().first,y=q.front().second; q.pop();
+            for(auto i:dir){
+                int x1=x+i[0] ,y1=y+i[1];
+                if(x1<0 || y1<0 || x1>=r || y1>=c) continue;
+                if(I[x1][y1]!=val) continue;
+                if(I[x1][y1]==C) continue;
+                I[x1][y1]=C;
+                q.push({x1,y1});
+            }
+        }
         return I;
     }
-    void dfs(vector<vector<int>>& I,int sr, int sc, int C,int val){
-        
-         if(sr<0||sc<0||sr>=I.size()||sc>=I[0].size()||I[sr][sc]==C||I[sr][sc]!=val){
-            return ;
-        }
-        I[sr][sc]=C;
-        dfs(I,sr+1,sc,C,val);
-        dfs(I,sr,sc+1,C,val);
-        dfs(I,sr-1,sc,C,val);
-        dfs(I,sr,sc-1,C,val);
-        return ;
-    }        
 };

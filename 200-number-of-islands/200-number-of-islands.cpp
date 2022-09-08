10069dfe03ff26f97f3e3dd1b99775671e97e611
Ankit Kumar <76895565+ankitkumar0624@users.noreply.github.com>
2022-09-08 11:36:19 +0530

    Time: 37 ms (91.84%), Space: 12.2 MB (82.26%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& g) {
-        int dir[4][2]={{1,0},{-1,0},{0,1},{0,-1}};
-        int m=g.size() ,n=g[0].size();
-        int sum=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        int cnt=0;
+        for(int i=0 ;i<g.size();i++){
+            for(int j=0 ;j<g[0].size();j++){
                 if(g[i][j]=='1'){
-                    queue<pair<int,int>>q;
-                    q.push({i,j});
-                    sum++;
-                   while(!q.empty()){
-                       auto cur=q.front();q.pop();
-                        for(auto x:dir){
-                            int r=x[0]+cur.first ,c=x[1]+cur.second;
-                            if(r<0 || c<0 || r>=m || c>=n || g[r][c]!='1') continue;
-                            g[r][c]='0';
-                            q.push({r,c});
-                        }
-                   }
+                    cnt++;
+                    dfs(g,i,j);
                 }
             }
         }
-        return sum;
+        return cnt;
+    }
+    void dfs(vector<vector<char>>& g ,int x,int y){
+        if(x>=0&&x<g.size()&&y>=0&&y<g[0].size()&&g[x][y]=='1'){
+            g[x][y]='0';
+        }else{
+            return;
+        }
+        dfs(g,x+1,y);
+        dfs(g,x-1,y);
+        dfs(g,x,y+1);
+        dfs(g,x,y-1);
+        return ;
     }
 };

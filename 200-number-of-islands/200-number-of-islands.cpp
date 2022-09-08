d64e2ec5a97028c6f5b229e5505f831692e9ec86
Ankit Kumar <76895565+ankitkumar0624@users.noreply.github.com>
2022-09-08 10:38:45 +0530

    Time: 73 ms (25.92%), Space: 18.2 MB (16.34%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
-       int islands=0 ,m=grid.size() ,n=grid[0].size();
-        int arr[]={0,1,0,-1,0};
-        for(int r=0 ;r<m ;r++){
-            for(int c=0 ;c<n ;c++){
-                if(grid[r][c]=='1'){
-                    islands++;
-                    grid[r][c]='0';
+    int numIslands(vector<vector<char>>& g) {
+        int dir[4][2]={{1,0},{-1,0},{0,1},{0,-1}};
+        int m=g.size() ,n=g[0].size();
+        int sum=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(g[i][j]=='1'){
                     queue<pair<int,int>>q;
-                    q.push({r,c});
-                    while(!q.empty()){
-                        auto cur=q.front() ;q.pop();
-                        int j=cur.first ,k=cur.second;
-                        for(int i=0 ;i<4 ;i++){
-                            int nr=j+arr[i] ,nc=k+arr[i+1];
-                            if(nr<0 || nc<0 || nc==n || nr==m || grid[nr][nc]!='1') continue;
-                            grid[nr][nc]='0';
-                            q.push({nr,nc});
+                    q.push({i,j});
+                    sum++;
+                   while(!q.empty()){
+                       auto cur=q.front();q.pop();
+                        for(auto x:dir){
+                            int r=x[0]+cur.first ,c=x[1]+cur.second;
+                            if(r<0 || c<0 || r>=m || c>=n || g[r][c]!='1') continue;
+                            g[r][c]='0';
+                            q.push({r,c});
                         }
-                    }
+                   }
                 }
             }
         }
-        return islands;
+        return sum;
     }
 };

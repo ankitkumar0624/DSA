9bb744cd6deb9269b31891d98b3ced2960c612d0
Ankit Kumar <76895565+ankitkumar0624@users.noreply.github.com>
2022-09-20 01:52:28 +0530

    Time: 191 ms (50.39%), Space: 44.5 MB (32.17%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    vector<vector<string>> findDuplicate(vector<string>& paths){
-        unordered_map<string,vector<string>>files;
-        vector<vector<string>>results;
-        for(auto&path:paths){
-            stringstream ss(path);
-            string s;
-            string root;
-            getline(ss,root,' ');
-            while(getline(ss,s,' ')){
-                string filename=root+'/'+s.substr(0,s.find('('));
-                string filecontents=s.substr(s.find('(')+1,s.find(')')-s.find('(')-1);
-                files[filecontents].push_back(filename);
-            }
-        }
-        for(auto i:files){
-            if(i.second.size()>1){
-                results.push_back(i.second);
-            }
-        }
-        return results;
+    vector<vector<string>> findDuplicate(vector<string>& paths) {
+    unordered_map<string, vector<string>> files;
+    vector<vector<string>> result;
+
+    for (auto path : paths) {
+	    stringstream ss(path);
+	    string root;
+	    string s;
+	    getline(ss, root, ' ');
+	    while (getline(ss, s, ' ')) {
+		    string fileName = root + '/' + s.substr(0, s.find('('));
+		    string fileContent = s.substr(s.find('(') + 1, s.find(')') - s.find('(') - 1);
+		    files[fileContent].push_back(fileName);
+	    }
     }
+
+    for (auto file : files) {
+	    if (file.second.size() > 1)
+		    result.push_back(file.second);
+    }
+
+    return result;
+}
 };

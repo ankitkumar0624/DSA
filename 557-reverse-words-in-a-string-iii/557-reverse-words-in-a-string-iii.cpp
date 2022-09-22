dd41cb8622b7402dff628ce73c31ebd2e993abc8
Ankit Kumar <76895565+ankitkumar0624@users.noreply.github.com>
2022-09-23 02:30:03 +0530

    Time: 12 ms (93.09%), Space: 9.8 MB (45.57%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     string reverseWords(string s) {
-      int l=0,r=0;
-        for(l,r;r<s.size()&&l<s.size();r++){
+        int l=0,r=0;
+        for(l,r;l<s.size()&&r<s.size();r++){
             if(s[r]==' '){
                 reverse(s.begin()+l,s.begin()+r);
-                if(r+1<s.size())l=r+1;
+                l=r+1;
             }
         }
-        if(l!=s.size()){
-            reverse(s.begin()+l,s.begin()+r);
-        }
+        reverse(s.begin()+l,s.begin()+r);
         return s;
     }
 };

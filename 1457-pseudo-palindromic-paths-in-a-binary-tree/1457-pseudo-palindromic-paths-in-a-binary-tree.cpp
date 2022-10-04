05ff1ecd1cfea203e699f3c6c75eb4a4bae6d056
Ankit Kumar <76895565+ankitkumar0624@users.noreply.github.com>
2022-10-04 11:11:09 +0530

    Time: 893 ms (20.41%), Space: 202.1 MB (20.42%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int cnts=0;
     unordered_map<int,int>mp;
+    int count=0;
     int pseudoPalindromicPaths (TreeNode* root) {
-        rec(root);
-        return cnts;
+        path(root);
+        return count;
     }
-    void rec(TreeNode*root){
-        if(!root) return;
+    void path(TreeNode*root){
+        if(root==NULL)return ;
         mp[root->val]++;
+        path(root->left);
+        path(root->right);
         if(!root->left&&!root->right){
-            int odd=0;
-            for(auto i:mp)if(i.second%2!=0)odd++;
-            if(odd<=1)cnts++;
+            int a=0;
+            for(auto i:mp)if(i.second%2!=0)a++;
+            if(a<2)count++;
         }
-        rec(root->left);
-        rec(root->right);
         mp[root->val]--;
         if(mp[root->val]==0)mp.erase(root->val);
-        return ;
+        return;
     }
 };

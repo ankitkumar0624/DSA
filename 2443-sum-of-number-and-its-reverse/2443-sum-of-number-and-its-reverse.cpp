17312a7b0d8696a3dfe15d0c85abe8a9dbdb428e
Ankit Kumar <76895565+ankitkumar0624@users.noreply.github.com>
2022-10-16 10:59:57 +0530

    Time: 48 ms (100.00%), Space: 5.9 MB (100.00%) - LeetHub

---
+++ 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool sumOfNumberAndReverse(int num) {
+        for(int i=num/2;i<=num;i++){
+            int ri=0;
+            int k=i;
+            while(k){
+                int last=k%10;
+                k/=10;
+                ri=ri*10+last;
+            }
+            if(i+ri==num)return 1;
+        }
+        return 0;
+    }
+};
